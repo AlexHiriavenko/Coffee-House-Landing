f7dc986f9aa5272e4036ff20cc00b90b37ed0063
Alex_March <martmarchmartmarch@gmail.com>
2026-09-20 12:36:46 +0300

    feat: add header and images

--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
+import './styles/style.scss';
 import './components/header/header.js';
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,30 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
+const base = '/Coffee-House-Landing/';
 const componentsPath = resolve(import.meta.dirname, 'src/components');
+const pages = {
+  home: resolve(import.meta.dirname, 'index.html'),
+  menu: resolve(import.meta.dirname, 'menu/index.html'),
+};
 
 export default defineConfig({
-  base: '/RSSchool/portfolio-example/',
+  base,
+  appType: 'mpa',
 
   plugins: [
     handlebars({
       partialDirectory: componentsPath,
+
+      context: {
+        basePath: base,
+      },
     }),
   ],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: pages,
+    },
+  },
 });
