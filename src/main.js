dc462a63cb6291dc7265dcf056b562e1b4661e87
Alex_March <martmarchmartmarch@gmail.com>
2026-09-24 22:36:09 +0300

    feat: implement slider functionality

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './styles/style.scss';
 import './components/shared/header/header.js';
 import './components/shared/header/theme-switch/theme-switch.js';
+import './components/home/favorites/slider/slider.js';
